82fc477b62a7ac20626a9365c843272724bbfc05
cdanielqf <cdanielqf@gmail.com>
2019-07-27 15:40:40 -0500

    Añadiendo un proyecto simple de calculadora

---
+++ src/componentes/Calculadora.java
@@ -0,0 +1,37 @@
+package componentes;
+
+public class Calculadora {
+	
+	public Calculadora() {
+	}
+
+	public long suma(long a, long b) {
+		return a + b;
+	}
+
+	public long resta(long a, long b) {
+		return a - b;
+	}
+
+	public long multiplicar(long a, long b) {
+		return a * b;
+	}
+
+	public long dividir(long a, long b) throws Exception{
+		if (b==0) throw new Exception("Division por 0");
+		return 0;
+	}
+	
+	public long potencia(long a, long b) throws Exception {
+		if (a==0 && b==0) throw new Exception("0 elevado a la 0");
+		return (long) Math.pow(a, b);
+	}
+	
+	public long factorial(long a) throws Exception {
+		if (a < 0) throw new Exception("Factorial de negativo");
+		if (a > 10) throw new Exception("Demasiado grande");
+		if (a <= 1) return 1;
+		return a * factorial(a-1);
+	}
+
+}
---
+++ src/test/CalculadoraTest.java
@@ -0,0 +1,118 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.hamcrest.CoreMatchers;
+import org.junit.Test;
+
+import componentes.Calculadora;
+
+public class CalculadoraTest {
+
+	@Test
+	public void testSuma() {
+		Calculadora calc = new Calculadora();
+		long resultado = calc.suma(4, 2);
+		long esperado = 6;
+		assertEquals(esperado, resultado);
+	}
+
+	@Test
+	public void testResta() {
+		Calculadora calc = new Calculadora();
+		long resultado = calc.resta(4, 2);
+		long esperado = 2;
+		assertEquals(esperado, resultado);
+	}
+
+	@Test
+	public void testMultiplicar() {
+		Calculadora calc = new Calculadora();
+		long resultado = calc.multiplicar(4, 2);
+		long esperado = 8;
+		assertEquals(esperado, resultado);
+	}
+
+	@Test
+	public void testDividir() {
+		Calculadora calc = new Calculadora();
+		long resultado = 0;
+		try {
+			resultado = calc.dividir(4, 2);
+		} catch (Exception e) {
+			fail("Error en la division normal");
+		}
+		long esperado = 2;
+		assertEquals(esperado, resultado);
+	}
+
+	@Test
+	public void testDividirPorCero() {
+		Calculadora calc = new Calculadora();
+		try {
+			calc.dividir(4, 0);
+			fail("Error: no lanza la excepcion al dividir por 0");
+		} catch (Exception e) {
+			assertThat(e.getMessage(), CoreMatchers.containsString("Division por 0"));
+		}
+	}
+
+	@Test
+	public void testPotencia() throws Exception {
+		Calculadora calc = new Calculadora();
+		long resultado = calc.potencia(4, 2);
+		long esperado = 16;
+		assertEquals(esperado, resultado);
+	}
+
+	@Test
+	public void testPotencia0a0() {
+		Calculadora calc = new Calculadora();
+		try {
+			calc.potencia(0, 0);
+			fail("Error: no lanza la excepcion al elevar 0 al a 0");
+		} catch (Exception e) {
+			assertThat(e.getMessage(), CoreMatchers.containsString("0 elevado a la 0"));
+		}
+	}
+
+	@Test
+	public void testFactorial() throws Exception {
+		Calculadora calc = new Calculadora();
+		long resultado = calc.factorial(0);
+		long esperado = 1;
+		assertEquals(esperado, resultado);
+
+		resultado = calc.factorial(1);
+		esperado = 1;
+		assertEquals(esperado, resultado);
+
+		resultado = calc.factorial(4);
+		esperado = 24;
+		assertEquals(esperado, resultado);
+	}
+	
+	@Test
+	public void testFactorialNegativo() {
+		Calculadora calc = new Calculadora();
+		try {
+			calc.factorial(-1);
+			fail();
+		} catch (Exception e) {
+			assertThat(e.getMessage(), CoreMatchers.containsString("Factorial de negativo"));
+		}
+	}
+	
+	@Test
+	public void testFactorialSobrecarga() {
+		Calculadora calc = new Calculadora();
+		try {
+			calc.factorial(20);
+			fail();
+		} catch (Exception e) {
+			assertThat(e.getMessage(), CoreMatchers.containsString("Demasiado grande"));
+		}
+	}
+	
+	
+}
